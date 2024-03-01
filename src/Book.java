49f82b0d458c296d6f07a69ab31f8c0a7f9298af
kkurs@mail.ru <kkurs@mail.ru>
2024-03-01 18:08:33 +0700

    Выполнено_ДЗ. Объекты и классы

---
+++ src/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String title;
+    private Author author;
+    private int yearPublication;
+
+    public Book (String title, Author author, int yearPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+    public int getYearPublication() {
+        return this.yearPublication;
+    }
+
+    public Author getAuthor () {
+        return this.author;
+    }
+
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        Author bulgakov = new Author("Михаил", "Булгаков");
+        Author kafka = new Author("Франц", "Кафка");
+        Book masterYmargarita = new Book("Мастер и Маргарита", bulgakov, 1992);
+        Book castle = new Book("Замок", kafka, 2002);
+        System.out.println("Книга - " + masterYmargarita.getTitle() + " Год издания - " + masterYmargarita.getYearPublication() + " Автор - " +
+                bulgakov.getName() + " " + bulgakov.getSurname());
+        System.out.println("Книга - " + castle.getTitle() + " Год издания - " + castle.getYearPublication() + " Автор - " +
+                kafka.getName() + " " + kafka.getSurname());
+
+        castle.setYearPublication(1995);
+
+        System.out.println("Книга - " + castle.getTitle() + " Год издания - " + castle.getYearPublication() + " Автор - " +
+                kafka.getName() + " " + kafka.getSurname());
 
     }
 }
